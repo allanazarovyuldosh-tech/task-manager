bef501a39ce866368851e2c5d11830df2eb5f188
allanazarovyuldosh-tech <allanazarovyuldosh@gmail.com>
2026-04-12 16:19:39 +0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,25 @@
 document.getElementById("app").innerHTML = `
   <p>Здесь будет список задач.</p>
 `;
+const input = document.getElementById("taskInput");
+const addBtn = document.getElementById("addTask");
+const list = document.getElementById("taskList");
+
+addBtn.addEventListener("click", () => {
+  const text = input.value.trim();
+  if (text !== "") {
+    const li = document.createElement("li");
+    li.textContent = text;
+
+    // кнопка удаления
+    const delBtn = document.createElement("button");
+    delBtn.textContent = "Удалить";
+    delBtn.style.marginLeft = "10px";
+    delBtn.onclick = () => li.remove();
+
+    li.appendChild(delBtn);
+    list.appendChild(li);
+    input.value = "";
+  }
+});
 
